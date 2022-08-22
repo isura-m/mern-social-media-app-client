8e7285fe2e0decab967abae2ddf0b867032c162c
isura <isuradev@icloud.com>
2022-08-22 09:08:21 +0530

    bug fixes

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const url = "http://localhost:5000/posts";
 
-export const fetchPosts = () => axios.get(url);
+export const fetchPosts = () => {
+  const result = axios.get(url);
+  console.log(result);
+  return result;
+};
 export const createPost = (newPost) => axios.post(url, newPost);
